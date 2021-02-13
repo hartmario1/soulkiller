302d6c4fab13586ac29e3e51190fb388f00614f9
didinele <didinele.dev@gmail.com>
2021-02-13 11:07:59 +0200

    chore: initial commit

---
+++ libs/common/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) Soulkiller App, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by the Soulkiller team <soulkilleroffice@gmail.com>, February 2021
+ * @license
+ */
+
+export {};
