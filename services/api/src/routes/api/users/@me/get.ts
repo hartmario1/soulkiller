hartmario1/soulkiller

commit b1423fa8ce3be2aee1aa13f1b17c7b733626f736

fix(frontend/backend): data fetching/confidential data inclusion

services/api/src/routes/api/users/@me/get.ts
@@ -10,7 +10,7 @@ export default class GetUserRoute extends Route {
     res.statusCode = 200;
     res.setHeader('content-type', 'application/json');
 
-    const { public_flags, premium_type, flags, verified, locale, mfa_enabled, system, bot, ...user } = req.user!;
+    const { public_flags, premium_type, flags, verified, locale, mfa_enabled, system, bot, email, id, ...user } = req.user!;
     return res.end(JSON.stringify(user));
   }
 }

services/dash/src/stores/user.ts
@@ -1,9 +1,7 @@
 import create, { State } from 'zustand';
 
 export interface ApiUserData {
-  id: string;
   avatar: string | null;
-  email: string;
   username: string;
   discriminator: string;
 }

services/dash/src/hooks/useQueryMe.ts
@@ -23,8 +23,12 @@ export const useQueryMe = () => {
   );
 
   const user = useUserStore();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => data && user.setUser(data), [data]);
+  useEffect(() => {
+    if (data) {
+      user.login();
+      user.setUser(data);
+    }
+  }, [data]); // eslint-disable-line react-hooks/exhaustive-deps
 
   if (error) return null;
   if (!data) return null;

commit 005bb53ebbe721ae900769d8abb40569b260a59d

fix: regression from b1423fa8ce3be2aee1aa13f1b17c7b733626f736

services/api/src/routes/api/users/@me/get.ts
@@ -10,7 +10,7 @@ export default class GetUserRoute extends Route {
     res.statusCode = 200;
     res.setHeader('content-type', 'application/json');
 
-    const { public_flags, premium_type, flags, verified, locale, mfa_enabled, system, bot, email, id, ...user } = req.user!;
+    const { public_flags, premium_type, flags, verified, locale, mfa_enabled, system, bot, email, ...user } = req.user!;
     return res.end(JSON.stringify(user));
   }
 }

services/dash/src/stores/user.ts
@@ -1,6 +1,7 @@
 import create, { State } from 'zustand';
 
 export interface ApiUserData {
+  id: string;
   avatar: string | null;
   username: string;
   discriminator: string;